1070333ea54cf9503bdca6983c7df2fa926c9721
Oleh Hedsiuk <oleg123hedsiuk@gmail.com>
2018-04-05 15:41:00 +0300

    domria export bugfix 3

--- src/AppBundle/Controller/Admin/ExportController.php
+++ src/AppBundle/Controller/Admin/ExportController.php
@@ -98,7 +98,7 @@ class ExportController extends Controller {
                         $arParams[$parent->getId()]['code'] = $parent->getExportName();
                         switch ($parent->getType()){
                             case 'select': 
-                                $val = $param->getProperty()->setExportName()!="" ? $param->getProperty()->setExportName() : $param->getProperty()->setName();
+                                $val = $param->getProperty()->getExportName()!="" ? $param->getProperty()->getExportName() : $param->getProperty()->getName();
                                 $arParams[$parent->getId()]['values'][] = $val;break;
                             case 'text': $arParams[$parent->getId()]['values'][] = $param->getString(); break;
                             case 'integer': $arParams[$parent->getId()]['values'][] = $param->getNumber(); break;
