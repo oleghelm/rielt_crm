67959896febc38ba518c8c486fba62c4f4200f29
Oleh Hedsiuk <oleg123hedsiuk@gmail.com>
2018-09-10 09:16:57 +0300

    parser dom ria 0.1.2

--- src/AppBundle/Controller/Admin/DomRiaParserController.php
+++ src/AppBundle/Controller/Admin/DomRiaParserController.php
@@ -44,10 +44,10 @@ class DomRiaParserController extends Controller {
         if(!empty($this->ids)){
             $this->getCompareCurrentObjectsList();
         }
-//        $this->addObjects();
-        $d_object = $this->getDomRiaObjectByID(14769144);
-        $this->addDomriaObject(14769144,$d_object);
-        die;
+        $this->addObjects();
+//        $d_object = $this->getDomRiaObjectByID(14769144);
+//        $this->addDomriaObject(14769144,$d_object);
+//        die;
             
 //        dump($this->ids);
 //        $this->getDomRiaObjectByID(14769144);
@@ -135,7 +135,7 @@ class DomRiaParserController extends Controller {
             $d_object = $this->getDomRiaObjectByID($id);
             $this->addDomriaObject($id,$d_object);
             $i++;
-            if($i>=5)die;
+            if($i>=10)die;
         }
     }
     
