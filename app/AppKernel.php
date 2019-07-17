6db2a38e5501673ffb502a219ec59bfb5352dc3b
Oleh Hedsiuk <oleg123hedsiuk@gmail.com>
2019-07-17 17:13:40 +0300

    add debuging for prod for me

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true) || $_COOKIE['isAdmin']=='Y') {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
